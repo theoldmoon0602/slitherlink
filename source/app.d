dad6ea462fa0128dbc308ffeaf72f82f2deb86ce
theoldmoon0602 <theoldmoon0602@gmail.com>
2018-06-21 22:03:47 +0900

    update

--- source/app.d
+++ source/app.d
@@ -6,6 +6,7 @@ import std.range;
 import std.math;
 import std.typecons;
 import std.traits;
+import std.format;
 
 enum YOKO = 0;
 enum TATE = 1;
@@ -13,6 +14,7 @@ enum TATE = 1;
 enum EMPTY = 0;
 enum EDGE = 1;
 enum FORBID = 2;
+enum INVALID = 3;
 
 string gridconv(int v, bool yoko)
 {
@@ -48,6 +50,10 @@ long distance4(long[] p1, long[] p2)
 {
   return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]);
 }
+long distance8(long[] p1, long[] p2)
+{
+  return max(abs(p1[0]-p2[0]), abs(p1[1]-p2[1]));
+}
 
 long get(const(int[][]) vs, long[] p, long[] dp = [0, 0])
 {
@@ -60,7 +66,7 @@ long get(const(int[][]) vs, long[] p, long[] dp = [0, 0])
   return 4;
 }
 
-long get(const(int[][][]) grid, long[] p, long[] dp = [0, 0])
+long get(const(int[][][]) grid, long[] p, long[] dp)
 {
   long y = p[0]; long x = p[1];
   long dy = dp[0]; long dx = dp[1];
@@ -72,11 +78,17 @@ long get(const(int[][][]) grid, long[] p, long[] dp = [0, 0])
   y += dy;
   x += dx;
 
-  if (grid[d].inrange(y, x)) {
-    return grid[d][y][x];
-  }
-  return 4;
+  return grid.get([d, y, x]);
 }
+
+long get(const(int[][][]) grid, long[] p)
+{
+  if (grid[p[0]].inrange(p[1], p[2])) {
+    return grid[p[0]][p[1]][p[2]];
+  }
+  return INVALID;
+}
+
 
 void set(ref int[][][] grid, long[] p, long[] dp, int v)
 {
@@ -90,19 +102,109 @@ void set(ref int[][][] grid, long[] p, long[] dp, int v)
   y += dy;
   x += dx;
 
-  if (grid[d].inrange(y, x)) {
-    grid[d][y][x] = v;
-  }
+  grid.set(d, y, x, v);
 }
 
-void print(int[][] vs, int[][][] grid)
+void set(ref int[][][] grid, long[] p, int v)
+{
+  grid.set(p[0], p[1], p[2], v);
+}
+
+void set(ref int[][][] grid, long d, long y, long x, int v)
+{
+  if (!grid[d].inrange(y, x)) {
+    return;
+  }
+  if (grid[d][y][x] != EMPTY && grid[d][y][x] != v) {
+    auto msg = "at: (%d %d %d) is %s but assigning %s".format(d, x, y, gridconv(grid[d][y][x], d == YOKO),  gridconv(v, d == YOKO));
+    throw new Exception("Program error: " ~ msg);
+  }
+  if (grid[d][y][x] == v) {
+    return;
+  }
+
+  grid[d][y][x] = v;
+
+  if (v == EDGE) {
+    for (auto i = 0; i < 2; i++) {
+      // MARK FORBID
+      foreach (e; scanedge(d, y, x, i)) {
+        if (grid.get(e) == EDGE) {
+          foreach (e2; scanedge(d, y, x, i).remove!(x => x == e)) {
+            grid.set(e2, FORBID);
+          }
+          break;
+        }
+      }
+    }
+    // DRAW EDGE IF ONLY ONE WAY REMAINED
+    for (auto i = 0; i < 2; i++) {
+      auto cnt = [0, 0, 0, 0];
+      foreach (e; scanedge(d, y, x, i)) {
+        cnt[grid.get(e)]++;
+      }
+
+      if (cnt[EDGE] == 0 && cnt[EMPTY] == 1) {
+        foreach (e; scanedge(d, y, x, i)) {
+          if (grid.get(e) == EMPTY) {
+            grid.set(e, EDGE);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+}
+
+void print(const(int[][][]) grid)
+{
+  foreach (y; 0..grid[0].length-1) {
+    writeln(grid[YOKO][y][0..$-1].map!(x => "+"~gridconv(x, true)).join("") ~ "+");
+    writeln(grid[TATE][y].map!(x => gridconv(x, false)).array.join(" ") ~ gridconv(grid[TATE][y][$-1], false));
+  }
+  writeln(grid[YOKO][$-1][0..$-1].map!(x => "+"~gridconv(x, true)).join("") ~ "+");
+}
+
+void print(const(int[][]) vs, const(int[][][]) grid)
 {
   // grid EDGE
   foreach (y; 0..vs.length) {
     writeln(grid[YOKO][y][0..$-1].map!(x => "+"~gridconv(x, true)).join("") ~ "+");
-    grid[TATE][y].map!(x => gridconv(x, false)).zip(vs[y].map!(numconv)).map!"a[0]~a[1]".array.join("").writeln;
+    writeln(grid[TATE][y].map!(x => gridconv(x, false)).zip(vs[y].map!(numconv)).map!"a[0]~a[1]".array.join("") ~ gridconv(grid[TATE][y][$-1], false));
   }
   writeln(grid[YOKO][$-1][0..$-1].map!(x => "+"~gridconv(x, true)).join("") ~ "+");
+}
+
+long[][] scanedge(long d, long y, long x, int i)
+{
+  if (d == YOKO) {
+    if (i == 0) {
+      return [
+        [TATE, y-1, x+1],
+        [YOKO, y, x+1],
+        [TATE, y, x+1],
+      ];
+    }
+    return [
+      [TATE, y-1, x],
+      [YOKO, y, x-1],
+      [TATE, y, x],
+    ];
+  }
+
+  if (i == 0) {
+    return [
+      [YOKO, y, x-1],
+      [TATE, y-1, x],
+      [YOKO, y, x],
+    ];
+  }
+  return [
+    [YOKO, y+1, x-1],
+    [TATE, y+1, x],
+    [YOKO, y+1, x],
+  ];
 }
 
 long[][] scan(int mode = -1) {
@@ -143,14 +245,12 @@ void try_surround3(ref int[][][] grid, long[] p)
     cnt[grid.get(p, dp)]++;
   }
 
-  bool is_surrounded = false;
   if (cnt[FORBID] == 1) {
     foreach (dp; scan) {
       if (grid.get(p, dp) != FORBID) {
         grid.set(p, dp, EDGE);
       }
     }
-    is_surrounded = true;
   }
   else if (cnt[EDGE] == 3 && cnt[EMPTY] == 1) {
     foreach (dp; scan) {
@@ -159,27 +259,10 @@ void try_surround3(ref int[][][] grid, long[] p)
         break;
       }
     }
-    is_surrounded = true;
-  }
-
-  if (is_surrounded) {
-    surrounded3(grid, p);
   }
 
 }
 
-void surrounded3(ref int[][][] grid, long[] p)
-{
-  foreach (dp; scan) {
-    if (grid.get(p, dp) == FORBID) {
-      foreach (rdp; rotdp(dp)) {
-        grid.set(p.shift(dp.rev).shift(rdp), dp, FORBID);
-        grid.set(p.shift(dp.rev).shift(rdp), rdp.rev, FORBID);
-      }
-      break;
-    }
-  }
-}
 
 void solve(const(int[][]) vs, ref int[][][] grid)
 {
@@ -209,14 +292,13 @@ void solve(const(int[][]) vs, ref int[][][] grid)
         foreach (op; scan.remove!(x => x == dp)) {
           grid.set(p, op, EDGE);  // other sides
         }
-        grid.surrounded3(p);  // forbid mark
         break;
       }
     }
   }
 
 
-  // three neighbored of another
+  // three neighbored of another one
   foreach (i; 0..threes.length) {
     foreach (j; (i+1)..threes.length) {
       // is neighbored?
@@ -231,6 +313,50 @@ void solve(const(int[][]) vs, ref int[][][] grid)
         try_surround3(grid, threes[i]);
         try_surround3(grid, threes[j]);
       }
+    }
+  }
+
+  // three 8-neighbored of another one
+  foreach (i; 0..threes.length) {
+    foreach (j; (i+1)..threes.length) {
+      if (distance4(threes[i], threes[j]) == 2 &&
+          distance8(threes[i], threes[j]) == 1) {
+        int up = (threes[i][0] < threes[j][0]) ? 1 : -1;
+        int left = (threes[i][1] < threes[j][1]) ? 1 : -1;
+
+        grid.set(threes[i], [-1 * up, 0], EDGE);
+        grid.set(threes[i], [0, -1 * left], EDGE);
+        grid.set(threes[j], [1 * up, 0], EDGE);
+        grid.set(threes[j], [0, 1 * left], EDGE);
+
+        try_surround3(grid, threes[i]);
+        try_surround3(grid, threes[j]);
+      }
+    }
+  }
+
+  // three at corner
+  foreach(p; threes) {
+    auto n = vs.length - 1;
+    if (p == [0, 0]) {
+      grid.set(p, [-1, 0], EDGE);
+      grid.set(p, [0, -1], EDGE);
+      try_surround3(grid, p);
+    }
+    else if (p == [0, n]) {
+      grid.set(p, [-1, 0], EDGE);
+      grid.set(p, [0, 1], EDGE);
+      try_surround3(grid, p);
+    }
+    else if (p == [n, 0]) {
+      grid.set(p, [1, 0], EDGE);
+      grid.set(p, [0, -1], EDGE);
+      try_surround3(grid, p);
+    }
+    else if (p == [n, n]) {
+      grid.set(p, [1, 0], EDGE);
+      grid.set(p, [0, 1], EDGE);
+      try_surround3(grid, p);
     }
   }
 }
